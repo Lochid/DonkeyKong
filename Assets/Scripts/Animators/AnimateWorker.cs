755e88f437dee66d5e1c629bc5e5ea997c2cf889
ndremov <ndremov@sfxdx.ru>
2019-12-28 01:57:03 +0300

    Add turning

--- Assets/Scripts/Animators/AnimateWorker.cs
+++ Assets/Scripts/Animators/AnimateWorker.cs
@@ -2,24 +2,45 @@
 
 [RequireComponent(typeof(IWalkMotion))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class AnimateWorker : MonoBehaviour
 {
 
     private MotionState _motionState;
     private IWalkMotion _walkMotion;
     private Animator animator;
+    private SpriteRenderer sprite;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         _motionState = new MotionState(animator);
         _walkMotion = GetComponent<IWalkMotion>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         AnimateIdle();
         AnimateWalk();
+        TurnLeft();
+        TurnRight();
+    }
+
+    private void TurnLeft()
+    {
+        if (_walkMotion.WalkLeft)
+        {
+            sprite.flipX = true;
+        }
+    }
+
+    private void TurnRight()
+    {
+        if (_walkMotion.WalkRight)
+        {
+            sprite.flipX = false;
+        }
     }
 
     private void AnimateIdle()
