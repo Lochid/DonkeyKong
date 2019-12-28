997720058c939c1005e1c60b0745926aa6682952
ndremov <ndremov@sfxdx.ru>
2019-12-28 23:51:47 +0300

    Fix upping by elevator

--- Assets/Scripts/Motions/States/MotionStates.cs
+++ Assets/Scripts/Motions/States/MotionStates.cs
@@ -2,23 +2,46 @@
 using UnityEngine;
 
 [RequireComponent(typeof(IRigidbodyAdapter))]
+[RequireComponent(typeof(IPhysicsAdapter))]
 public class MotionStates : MonoBehaviour, IWalkMotionState, IJumpMotionState
 {
+    [SerializeField]
+    private Vector2 positionOffset = new Vector2(0, 33);
+    [SerializeField]
+    private float raycastOffset = 0.00005f;
+
     public bool WalkLeft => rigidbodyAdapter.velocity.x < -0.5f;
     public bool WalkRight => rigidbodyAdapter.velocity.x > 0.5f;
     public bool Walk => WalkLeft || WalkRight;
-    public bool Fall => Mathf.Abs(rigidbodyAdapter.velocity.y) > 1f;
+    public bool Fall
+    {
+        get
+        {
+            var withVerticalSpeed = Mathf.Abs(rigidbodyAdapter.velocity.y) > 1f;
+            if (!withVerticalSpeed)
+            {
+                return withVerticalSpeed;
+            }
+
+            var hit = physicsAdapter.Raycast(rigidbodyAdapter.position + positionOffset, Vector2.down, raycastOffset);
+
+            return hit.collider == null;
+        }
+    }
+
     public bool Stop => !Walk && !Fall;
     public bool Land { get; private set; } = false;
 
 
     private IRigidbodyAdapter rigidbodyAdapter;
+    private IPhysicsAdapter physicsAdapter;
 
     private bool prevFall;
 
     private void Start()
     {
         rigidbodyAdapter = GetComponent<IRigidbodyAdapter>();
+        physicsAdapter = GetComponent<IPhysicsAdapter>();
     }
 
     private void FixedUpdate()
