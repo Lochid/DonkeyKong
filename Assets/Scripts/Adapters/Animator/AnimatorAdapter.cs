74de752d65662783c15160c7ec9dc9332c3cfabc
Valery Chirkov <yvalch@yandex.ru>
2020-01-11 16:32:05 +0300

    Upd prev bug fix via scripts exec order

--- Assets/Scripts/Adapters/Animator/AnimatorAdapter.cs
+++ Assets/Scripts/Adapters/Animator/AnimatorAdapter.cs
@@ -5,15 +5,14 @@ public class AnimatorAdapter : MonoBehaviour, IAnimatorAdapter
 {
     private Animator animator;
 
-    /*
+    
     private void Start() //doesn't init correctly
     {
         animator = GetComponent<Animator>();
-    }*/
+    }
 
     public void SetTrigger(string trigger)
     {
-        if (animator == null) animator = GetComponent<Animator>();
         animator.SetTrigger(trigger);
     }
 }
