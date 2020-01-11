6403fd5faf1baa2b5d8dbb4fe636246bfae6dcfc
Valery Chirkov <yvalch@yandex.ru>
2020-01-11 16:12:15 +0300

    Fixed AnimatorAdapter init bug

--- Assets/Scripts/Adapters/Animator/AnimatorAdapter.cs
+++ Assets/Scripts/Adapters/Animator/AnimatorAdapter.cs
@@ -5,13 +5,15 @@ public class AnimatorAdapter : MonoBehaviour, IAnimatorAdapter
 {
     private Animator animator;
 
-    private void Start()
+    /*
+    private void Start() //doesn't init correctly
     {
         animator = GetComponent<Animator>();
-    }
+    }*/
 
     public void SetTrigger(string trigger)
     {
+        if (animator == null) animator = GetComponent<Animator>();
         animator.SetTrigger(trigger);
     }
 }
